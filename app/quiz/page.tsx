f944f2e0e176b28fadaf89f018f11b5045743e2e
VenessaKarlsson <112473165+VenessaKarlsson@users.noreply.github.com>
2025-12-16 10:28:59 +0100

    frågor och dummy data för svaren klara

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,8 +1,79 @@
+'use client'
+import { useState } from 'react'
+import { questions } from '../../data/questions'
+import styles from '../../styles/Quiz.module.css'
+import Link from 'next/link'
+
 export default function QuizPage() {
+  const [current, setCurrent] = useState(0)
+
+  const handleNext = () => {
+    if (current < questions.length - 1) {
+      setCurrent(current + 1)
+      setSelectedAnswer(null) // ← nollställer färgerna för nästa fråga
+    }
+  }
+
+  const question = questions[current]
+
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null)
+
+  const handleAnswerClick = (answer: string) => {
+    if (selectedAnswer) return
+    setSelectedAnswer(answer)
+  }
+
   return (
-    <main style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Quiz-sidan fungerar! 🎉</h1>
-      <p>Om du ser detta har länken från startsidan funkat.</p>
-    </main>
+    <>
+      <Link href="/">
+        <button className="homeButton">Hem</button>
+      </Link>
+      <main className={styles.quizWrapper}>
+        <div className={styles.quizContainer}>
+          {/* Bild till vänster */}
+          <div className={styles.imageContainer}>
+            <img
+              src={question.image}
+              alt="Frågebild"
+              className={styles.image}
+            />
+          </div>
+
+          <div className={styles.optionsWrapper}>
+            <h2 className={styles.questionText}>{question.text}</h2>
+
+            <div className={styles.options}>
+              {question.options.map((option) => {
+                let className = styles.optionBtn
+
+                if (selectedAnswer) {
+                  if (option === question.correctAnswer) {
+                    className += ` ${styles.correct}`
+                  } else if (option === selectedAnswer) {
+                    className += ` ${styles.wrong}`
+                  }
+                }
+
+                return (
+                  <button
+                    key={option}
+                    className={className}
+                    onClick={() => handleAnswerClick(option)}
+                    disabled={!!selectedAnswer}
+                  >
+                    {option}
+                  </button>
+                )
+              })}
+            </div>
+            {current < questions.length - 1 && (
+              <button className={styles.nextBtn} onClick={handleNext}>
+                Nästa
+              </button>
+            )}
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
---
+++ data/questions.ts
@@ -0,0 +1,241 @@
+export const questions = [
+  {
+    id: 1,
+    text: 'Venessa fick vad i huvudet på sin första fotbollsträning?',
+    image: '/1.jpg', // lägg bilden i public/
+    options: ['Stockholm', 'Göteborg', 'Malmö', 'Uppsala'],
+    correctAnswer: 'Stockholm'
+  },
+
+  {
+    id: 2,
+    text: 'Vilka fritidsaktiviteter har Dennis gjort?',
+    image: '/2.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 3,
+    text: 'Vilken biofilmpremiär tittade Debbie på här?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 4,
+    text: 'Vilken bilmodell kör Soroor just nu?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Gul'
+  },
+
+  {
+    id: 5,
+    text: 'I vilken stad bodde Nicklas i i några år',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 6,
+    text: 'Vad heter skolan Mikael jobbar på?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 7,
+    text: 'Vilken bilmodell kör Nicklas?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 8,
+    text: 'Vems favoritgodis är detta?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 9,
+    text: 'Vilka fritidsaktiviteter gjorde Venessa?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 10,
+    text: 'Vad heter företaget Sorror jobbar på?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 11,
+    text: 'Vad heter utbildningen som Nafisa läser?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Gul'
+  },
+
+  {
+    id: 12,
+    text: 'Hur mycket pengar spenderar Nicklas på monster varje år?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 13,
+    text: 'Venessa lånade Dennis ________ och ramlade. Vad lånade hon? ',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 13,
+    text: 'Vilken hundras är Keyla? ',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Gul'
+  },
+
+  {
+    id: 14,
+    text: 'Nämen hundens namn och Ras Bild på mig och King ',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 15,
+    text: 'Dessa två syskon var födda på nittio-talet. Bild på mig och Dennis när vi var små Nämn 3 saker som var stora på 90-talet ',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 16,
+    text: 'Sorrors betyder något på persiska? Vad är det?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 17,
+    text: 'Nicklas favoritfärg är blå. Deborahs favoritfärg är rosa. Om man blandar ihop Blå och rosa, vilken färg blir det?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 18,
+    text: 'Nämn alla filmer dessa karaktärer kommer ifrån Woody, Mofasa, Ash Cathem och Lilo',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 19,
+    text: 'Nämn Debbie favoriträtt på Chop Chop',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 20,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 21,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Gul'
+  },
+
+  {
+    id: 22,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 23,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 24,
+    text: 'Vart är vi påväg?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 25,
+    text: 'Vart är Sorror?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Gul'
+  },
+
+  {
+    id: 26,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Grön'
+  },
+
+  {
+    id: 27,
+    text: 'Vilken restaurangen är Debbie på?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Röd'
+  },
+
+  {
+    id: 28,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Blå'
+  },
+
+  {
+    id: 29,
+    text: 'Vart är vi?',
+    image: '/3.jpg',
+    options: ['Blå', 'Grön', 'Röd', 'Gul'],
+    correctAnswer: 'Gul'
+  }
+]
